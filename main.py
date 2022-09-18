bd6a5a2d6f1ba134c17b9a5e48ed93e12dda943a
Iliya Sergeev <50948446+lapatun1980@users.noreply.github.com>
2022-09-18 17:10:29 +0000

    Home Work 2

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 # Задание 1. Факториал
 
 def fac(n):
-
-  return n
+    facl=1
+    for i in range(2,n+1):
+         facl*=i   
+    return facl
 
 # Задание 2. Наибольший общий делитель (НОД) для двух целых чисел
 
-def gcd(a, b):    
-  pass
+def gcd(a, b):   
+    while(a!=0 and b!=0):
+        if a>b:
+            a=a%b
+        else: b=b%a
+    return (a+b)
+      
 
 # Задание 3. Генератор для ряда Фибоначчи
 
-def fib():
-   
+# def fib():
+#     a,b=1,1
+#     while True:
+#        yield  a
+#        a,b=b,a+b
+
+    
+
 
 
 # Задание 4. Напишите реализацию функции is_palindrome, которая проверяет, является ли последовательность палиндромом
@@ -25,7 +38,12 @@ def fib():
 # True
 
 def is_palindrome(seq):
-  return 
+    pal=seq[::-1]
+    if pal==seq:
+        return True
+    else: return False
+    
+     
 
 
 # Задание 5. Напишите реализацию функции is_unique, которая проверяет, содержит ли последовательность только уникальные элементы.
@@ -35,7 +53,11 @@ def is_palindrome(seq):
 # True
 
 def is_unique(seq):
-  return 0
+    s=set(seq)
+    if len(s)==len(seq):
+        return True
+    else:
+        return False
 
 
 # Задание 6. Напишите реализацию функции, которая вычисляет контрольную сумму последовательности чисел по модулю 10. Аргумент -- строку, содержащую только цифры, -- необходимо преобразовать в последовательность чисел, вычислить их сумму, и вернуть остаток от деления на 10.
@@ -49,7 +71,12 @@ def is_unique(seq):
 # 5
 
 def checksum(code):
-  return 0
+    # code=int(code)
+    code_l=list(code)
+    code_ld=[int(_) for _ in code_l]
+    print (code_ld)
+    code_sum=sum(code_ld)
+    return code_sum%10
 
 
 # Задание 7*. Преобразование вложенных последовательностей
@@ -64,8 +91,16 @@ def checksum(code):
 # [1, 2, 3, 4]
 
 def flatten(seq):
-  res = []
-
-  return res
-
-def xflatten(seq): 
+    res = []
+    for i in seq:
+        print(type(i))
+        # приводим к списку
+        if type(i)== set or type(i)==tuple:
+            i=list(i)
+        # Добавляем к списку элемент
+        if type(i) != list:
+            res.append(i)
+        # иначе вызываем рекурсию
+        else:
+            res += flatten(i) 
+    return res
